973b35b46949113e90118974379cd9e047af4e67
Ashiqur Rahman <ashikrobi@gmail.com>
2021-01-27 13:56:24 +0600

    completed withdraw button event handler without refactoring

--- js/west-bank.js
+++ js/west-bank.js
@@ -6,12 +6,32 @@ document.getElementById('login').addEventListener('click', function(){
 //deposit button event handler
 document.getElementById('add-deposit').addEventListener('click', function(){
     let depositInNumber = parseFloat(document.getElementById('deposit-amount').value);
-    let currentDepositNumber = parseFloat(document.getElementById('current-deposit').innerText);
-    let total = depositInNumber + currentDepositNumber;
-    document.getElementById('current-deposit').innerText = total;
+    // let currentDepositNumber = parseFloat(document.getElementById('current-deposit').innerText);
+    // let total = depositInNumber + currentDepositNumber;
+    // document.getElementById('current-deposit').innerText = total;
+    //we made a function with commented codes sow we can eliminate repetition
+    updateSpanText('current-deposit', depositInNumber);
     document.getElementById('deposit-amount').value = '';
     //update balance as well
-    let currentBalanceInNumber = parseFloat(document.getElementById("current-balance").innerText);
-    let updatedBalance = currentBalanceInNumber + depositInNumber;
-    document.getElementById('current-balance').innerText = updatedBalance;
+    updateSpanText('current-balance', depositInNumber);
 })
+
+//withdraw button event handler
+document.getElementById('add-withdraw').addEventListener('click', function(){
+    // let withdrawInNumber = parseFloat(document.getElementById('withdraw-amount').value);
+    let withdrawAmountInNumber = getInputNumber('withdraw-amount');
+    updateSpanText('current-withdraw', withdrawAmountInNumber);
+    updateSpanText('current-balance', -1 * withdrawAmountInNumber);
+    
+    document.getElementById('withdraw-amount').value = '';
+})
+function getInputNumber(id) {
+    let inputNumber = parseFloat(document.getElementById(id).value);
+    return inputNumber;
+}
+
+function updateSpanText(id , depositInNumber) {
+    let currentNumber = parseFloat(document.getElementById(id).innerText);
+    let total = currentNumber + depositInNumber;
+    document.getElementById(id).innerText = total;
+}
